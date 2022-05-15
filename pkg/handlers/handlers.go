43b333e7a3d66ca4ee0f93aa87d614696548f611
Soroush <s_shemshadi@ymail.com>
2022-05-15 22:25:59 +0430

    UPDATE(handlers.go) Implement a method to catch exceptions and extract their http status codes to return APIGatewayProxyRequest based on their error code.

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -6,13 +6,15 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 	"github.com/shuoros/go-challenge/pkg/device"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 func AddDevice(req events.APIGatewayProxyRequest, table string, dynaClient dynamodbiface.DynamoDBAPI) (
 	*events.APIGatewayProxyResponse, error) {
 	result, err := device.CreateDevice(req, table, dynaClient)
 	if err != nil {
-		return errorResponse(http.StatusBadRequest, aws.String(err.Error()))
+		return catchException(err)
 	}
 
 	return successResponse(http.StatusCreated, result)
@@ -24,7 +26,7 @@ func GetDevice(req events.APIGatewayProxyRequest, table string, dynaClient dynam
 
 	result, err := device.FetchDevice(deviceId, table, dynaClient)
 	if err != nil {
-		return errorResponse(http.StatusBadRequest, aws.String(err.Error()))
+		return catchException(err)
 	}
 
 	return successResponse(http.StatusOK, result)
@@ -32,4 +34,14 @@ func GetDevice(req events.APIGatewayProxyRequest, table string, dynaClient dynam
 
 func UnhandledMethod() (*events.APIGatewayProxyResponse, error) {
 	return errorResponse(http.StatusMethodNotAllowed, "Method Not Allowed")
+}
+
+func catchException(err error) (*events.APIGatewayProxyResponse, error) {
+	arr := strings.Split(err.Error(), "-")
+	if len(arr) == 2 {
+		statusCode, _ := strconv.Atoi(arr[0])
+		return errorResponse(statusCode, aws.String(arr[1]))
+	} else {
+		return errorResponse(http.StatusInternalServerError, aws.String(err.Error()))
+	}
 }
